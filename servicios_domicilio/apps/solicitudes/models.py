85d878e875aa9678e89799b12f05c000d4ec1f7e
unknown <joaquincaviedes3@gmail.com>
2025-05-22 21:09:58 -0300

    Se corrigio la estructura y se definieron serializadores y el viewset de usuario

---
+++ servicios_domicilio/apps/solicitudes/models.py
@@ -0,0 +1,39 @@
+from django.utils import timezone
+from django.db import models
+
+from django.conf import settings
+
+# Create your models here.
+class Solicitudes(models.Model):
+    ESTADO = [
+        ('pendiente', 'Pendiente'),
+        ('confirmada', 'Confirmada'),
+        ('finalizada', 'Finalizada')
+    ]
+    cliente = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        related_name='solicitudes_cliente', 
+        on_delete=models.CASCADE 
+    )
+    trabajador = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        related_name='solicitudes_trabajador', 
+        on_delete=models.CASCADE 
+    )
+    servicio = models.ForeignKey(
+        'servicios.Servicio', 
+        on_delete=models.CASCADE
+    )
+    direccion = models.CharField(
+        max_length=250, 
+        blank=False, 
+        unique=True
+    )
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_solicitada = models.DateField(default=timezone.now)
+    estado = models.CharField(max_length=20, choices=ESTADO, default='pendiente')
+
+    descripcion = models.TextField(max_length=1000, blank=True)
+
+    def __str__(self):
+        return f'{self.servicio.nombre} - {self.cliente.username} - {self.trabajador.username} - {self.estado}'
---
+++ servicios_domicilio/routers.py
@@ -0,0 +1,9 @@
+from rest_framework import routers 
+from apps.usuarios.api import UsuarioViewSet
+
+# Initializarel router de DRF solo una vez
+router = routers.DefaultRouter() 
+# Registrar un ViewSet
+router.register(prefix='usuarios', viewset=UsuarioViewSet)
+
+urlpatterns = router.urls
